7451926d101c8a21371ef99d2e056283c643e00a
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2022-07-07 04:14:08 +0530

    updated

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from PIL import Image
 # importingthe files
 from ml import ml
 
+img3 = Image.open("png.jpg")
+st.set_page_config(page_title = "Breakup Predictor", page_icon = img3)
 def main():
 		
 	st.write("# Breakup Predictor")
